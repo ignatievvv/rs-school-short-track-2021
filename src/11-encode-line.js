21365c8a2f7b074f562db590dff598ec5be93f8c
ignatievvv <ramorusbadrobot@gmail.com>
2021-04-12 01:21:28 +0300

    init: start encode-line subtask

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,21 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const array = [];
+  let counter = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i + 1] === str[i]) {
+      counter++;
+    } else if (counter === 1) {
+      array.push(str[i]);
+      counter = 1;
+    } else {
+      array.push(`${counter}${str[i]}`);
+      counter = 1;
+    }
+  }
+  return array.join('');
 }
 
 module.exports = encodeLine;
