02eff24fd194d6101afed297ef5eaa738ef93355
ignatievvv <ramorusbadrobot@gmail.com>
2021-04-11 15:44:10 +0300

    init: start mine-sweeper suntask

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,59 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const matrixNew = [];
+  for (let i = 0; i < matrix.length; i++) {
+    matrixNew.push([]);
+    for (let k = 0; k < matrix[0].length; k++) {
+      matrixNew[i].push(0);
+    }
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let k = 0; k < matrix[i].length; k++) {
+      if (k - 1 > -1) {
+        if (matrix[i][k - 1] === true) {
+          matrixNew[i][k]++;
+        }
+      }
+      if (k + 1 < matrix[i].length) {
+        if (matrix[i][k + 1] === true) {
+          matrixNew[i][k]++;
+        }
+      }
+      if (i + 1 < matrix.length && k - 1 > -1) {
+        if (matrix[i + 1][k - 1] === true) {
+          matrixNew[i][k]++;
+        }
+      }
+      if (i + 1 < matrix.length && k + 1 < matrix[i].length) {
+        if (matrix[i + 1][k + 1] === true) {
+          matrixNew[i][k]++;
+        }
+      }
+      if (i - 1 > -1 && k - 1 > -1) {
+        if (matrix[i - 1][k - 1] === true) {
+          matrixNew[i][k]++;
+        }
+      }
+      if (i - 1 > -1 && k + 1 < matrix[i].length) {
+        if (matrix[i - 1][k + 1] === true) {
+          matrixNew[i][k]++;
+        }
+      }
+      if (i + 1 < matrix.length) {
+        if (matrix[i + 1][k] === true) {
+          matrixNew[i][k]++;
+        }
+      }
+      if (i - 1 > -1) {
+        if (matrix[i - 1][k] === true) {
+          matrixNew[i][k]++;
+        }
+      }
+    }
+  }
+  return matrixNew;
 }
 
 module.exports = minesweeper;
