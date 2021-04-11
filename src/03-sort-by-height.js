a28c1a22cf75502a71d3e3ac38ab95579ca82c9a
ignatievvv <ramorusbadrobot@gmail.com>
2021-04-11 12:57:26 +0300

    init: start sort-by-height subtask

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,15 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  let NEWARR = arr.filter((elem) => elem !== -1);
+  NEWARR = NEWARR.sort((a, b) => a - b);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === -1) {
+      NEWARR.splice(i, 0, arr[i]);
+    }
+  }
+  return NEWARR;
 }
 
 module.exports = sortByHeight;
