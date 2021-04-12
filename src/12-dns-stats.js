2301df1a2cdb3083dcae034d29ea457a166362b7
ignatievvv <ramorusbadrobot@gmail.com>
2021-04-12 20:53:41 +0300

    init: start dns-stats subtask

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,40 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const domainsObj = {};
+  domains.forEach((elem) => {
+    if (/.\p{L}+$/u.test(elem)) {
+      const result = elem.match(/.\p{L}+$/u);
+      if (domainsObj[result[0]]) {
+        domainsObj[result[0]]++;
+      } else {
+        domainsObj[result[0]] = 1;
+      }
+    }
+    if (/\p{L}+\.\p{L}+$/u.test(elem)) {
+      const result0 = elem.match(/\.\p{L}+$/u);
+      const result = elem.match(/\p{L}+(?=\.\p{L}+$)/u);
+      const dot = '.';
+      if (domainsObj[result0[0] + dot + result[0]]) {
+        domainsObj[result0[0] + dot + result[0]]++;
+      } else {
+        domainsObj[result0[0] + dot + result[0]] = 1;
+      }
+    }
+    if (/^\p{L}+\.\p{L}+\.\p{L}+$/u.test(elem)) {
+      const dot = '.';
+      const result0 = elem.match(/\.\p{L}+$/u);
+      const result1 = elem.match(/\.\p{L}+(?=\.\p{L}+$)/u);
+      const result = elem.match(/^\p{L}+(?=\.\p{L}+\.\p{L}+$)/u);
+      if (domainsObj[result0[0] + result1[0] + dot + result[0]]) {
+        domainsObj[result0[0] + result1[0] + dot + result[0]]++;
+      } else {
+        domainsObj[result0[0] + result1[0] + dot + result[0]] = 1;
+      }
+    }
+  });
+  return domainsObj;
 }
 
 module.exports = getDNSStats;
